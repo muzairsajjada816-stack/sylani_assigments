98ed4a0f96650f1c45ad3a48bb685281970546d7
muzairsajjada816-stack <muzairsajjada816@gmail.com>
2026-06-02 16:51:28 +0500

    Added Assignment 01

---
+++ Assigment 1/index.js
@@ -0,0 +1,4 @@
+// Assigment Code:
+console.log('My name is M.Uzair');
+console.log('I am 14 years old');
+console.log('My Favorite language was Python but now it is JavaScript');
---
+++ Assigment 1/main.py
@@ -0,0 +1,3 @@
+print("My name is M.Uzair")
+print("I live in Karachi")
+print("My Favorite Hobby is gaming(playing Minecraft bedwars)")
