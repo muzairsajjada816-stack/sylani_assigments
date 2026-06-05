773d52ab426fab72bb76086951584cca20d28acd
muzairsajjada816-stack <muzairsajjada816@gmail.com>
2026-06-05 15:44:32 +0500

    Added Assignment 02

---
+++ Assignment 2.py
@@ -0,0 +1,41 @@
+# Task 1
+Name = 'Uzair'
+Age = 14
+City = 'Karachi'
+Student = True
+print(Name)
+print(Age)
+print(City)
+print(Student)
+
+# Task 2
+name = 'uzair'
+num = 123
+IS_STUDENT = True
+print(type(name))
+print(type(num))
+print(type(IS_STUDENT))
+
+# Task 3
+marks = int("95")
+print(int(marks))
+print(type(marks))
+
+# Task 4
+phoneNumber = str(123456789)
+print(phoneNumber)
+print(type(phoneNumber))
+
+# Task 5
+userName = bool("Ahmed")
+print(type(userName))
+
+# Task 6
+Name = 'Uzair'
+Age = 14
+Favorite_Color = 'Cyan'
+Student_Status = True
+print(Name)
+print(Age)
+print(Favorite_Color)
+print(Student_Status)
