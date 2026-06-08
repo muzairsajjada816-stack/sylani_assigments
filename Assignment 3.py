1b9bb01eaa507962b4d2b881d54a54609434d252
muzairsajjada816-stack <muzairsajjada816@gmail.com>
2026-06-08 16:48:24 +0500

    Added Assignment 03

---
+++ Assignment 3.py
@@ -0,0 +1,41 @@
+# Task 1
+
+print("My name is Uzair")
+print("I am learning Python")
+print("Python is Fun")
+
+# Task 2 
+
+Name = "Uzair"
+Age = 14
+City = "Karachi"
+print(Name)
+print(Age)
+print(City)
+
+# Task 3
+
+num1 = 10
+num2 = 15
+print(num1 + num2)
+
+# Task 4
+
+a = 5
+b = 3
+print(a * b)
+
+# Task 5
+
+print(20+10)
+print(20 - 10)
+print(20 * 10)
+print(20 / 10)
+print(20 % 3)
+print(20 // 3)
+print(2 ** 4)
+
+# bonus Task
+
+Favourite_number = 4
+print("My favourite number is:", Favourite_number)
