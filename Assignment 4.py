20bd72b5e304f3165ba7c537be0e64f7279bcf77
muzairsajjada816-stack <muzairsajjada816@gmail.com>
2026-06-09 16:29:24 +0500

    Added Assignment 03

---
+++ Assignment 4.py
@@ -0,0 +1,80 @@
+# Task 1
+a = 10
+b = 20
+if a > b:
+    print("a is greater than b")
+
+# Task 2
+name1= "Alice"
+name2= "Bob"
+if name1 == name2:
+    print("The names are the same.")
+
+# Task 3
+num1 = 20
+num2 = 30
+if num1 < num2:
+    print("First number is less than second number")
+
+# Task 4
+age = 18
+if age >= 18:
+    print("You can vote.")
+
+# Task 5
+marks = 85
+if marks >= 50:
+    print("You passed the exam.")
+else:
+    print("You failed the exam.")
+
+# Task 6
+num3 = 15
+num4 = 25
+if num3 > num4:
+    print("First number is greater than second number")
+else:    
+    print("First number is not greater than second number")
+
+# Task 7
+empty_variable = ""
+if not empty_variable: 
+    print("The variable is empty.")
+else:    
+    print("The variable is not empty.")
+    print("Plz provide a value for the variable.")
+
+# Task 8
+Name = "Uzair"
+if Name:
+    print("The Name is available.")
+
+# Task 9
+#Truthy value or falsy value
+value = 0
+if value:
+    print("The value is truthy.")   
+else:
+    print("The value is falsy.")
+
+# Task 10
+number = 5
+if number:
+    print("The number is truthy.")
+else:
+    print("The number is falsy.")
+
+# Task 11
+marks = 100
+if marks >= 80:
+    print("Grade: A")
+else:
+    print("Need improvement in marks.")
+
+# Task 12
+Username = "Uzair"
+Password = "12345"
+if Username == "Uzair" and Password == "12345":
+    print("Login successful.")
+else:    
+    print("Invalid username or password.")
